5e51f84b316dd480cb62da391304b57ecf67f70a
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-22 15:24:29 -0700

    solution found

--- Solution.java
+++ Solution.java
@@ -1,5 +1,20 @@
 public class Solution {
     public ListNode removeElements(ListNode head, int val) {
-        return ListNode.generateList(new int[] {});
+        ListNode ansPointer = new ListNode(-1, head);
+        ListNode prevPointer = ansPointer;
+        ListNode nextPointer = prevPointer.next;
+
+        while (nextPointer != null) {
+            if (nextPointer.val != val) {
+                prevPointer.next = nextPointer;
+                prevPointer = prevPointer.next;
+            }
+
+            nextPointer = nextPointer.next;
+        }
+
+        prevPointer.next = null;
+
+        return ansPointer.next;
     }
 } 
