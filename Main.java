1eef1a02f77b4416297c4e4455041a51b4ba4b78
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-22 14:55:56 -0700

    main method implemented

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        Solution sol = new Solution();
+        ListNode head1 = ListNode.generateList(new int[] { 1, 2, 6, 3, 4, 5, 6 });
+        ListNode head2 = ListNode.generateList(new int[] {});
+        ListNode head3 = ListNode.generateList(new int[] { 7, 7, 7, 7 });
 
+        System.out.println(sol.removeElements(head1, 6));
+        System.out.println(sol.removeElements(head2, 1));
+        System.out.println(sol.removeElements(head3, 7));
     }
+
+
 } 
