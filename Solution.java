e90d94c783300f8af59c8100cef09f5ed20bd2ec
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-22 15:38:10 -0700

    memory effcient without using System.gc()

--- Solution.java
+++ Solution.java
@@ -1,21 +1,9 @@
 public class Solution {
     public ListNode removeElements(ListNode head, int val) {
-        if (head == null || head.next == null)
-            return head;
-
-        ListNode current = head;
-
-        while (current.next != null) {
-            if (current.next.val == val) {
-                current.next = current.next.next;
-            } else {
-                current = current.next;
-            }
+        if (head == null) {
+            return null;
         }
-        if (head.val == val) {
-            head = head.next;
-        }
-
-        return head;
+        head.next = removeElements(head.next, val);
+        return head.val == val ? head.next : head;
     }
 } 
